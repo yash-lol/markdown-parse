5871373179390ef15c345a76d47d269608c7a0a2
yash-lol <yjoshi@ucsd.edu>
2022-01-27 15:57:52 -0800

    junit test

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,11 +12,14 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            if(nextOpenBracket == -1)
+                break;
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            
         }
         return toReturn;
     }
@@ -25,5 +28,7 @@ public class MarkdownParse {
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
+        
     }
 }
+
---
+++ firstline.md
@@ -0,0 +1,1 @@
+[link]twitch.tv
