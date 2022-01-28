bf403221ae09b43d52c8d0be1aec499a6035d268
yash-lol <yjoshi@ucsd.edu>
2022-01-28 11:56:04 -0800

    Test error while testing img.md
    1) testImg(MarkdownParseTest)
    java.lang.AssertionError: Test the links from img.md expected:<[]> but was:<[img.png]>
            at org.junit.Assert.fail(Assert.java:89)
            at org.junit.Assert.failNotEquals(Assert.java:835)
            at org.junit.Assert.assertEquals(Assert.java:120)
            at MarkdownParseTest.testImg(MarkdownParseTest.java:30)

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,3 +1,4 @@
+
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,25 +11,29 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        while (currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            if(nextOpenBracket == -1)
+            if (nextOpenBracket == -1)
                 break;
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if (nextOpenBracket != 0 && markdown.charAt(nextOpenBracket - 1) == '!') {
+                currentIndex = closeParen + 1;
+                continue;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-            
+
         }
         return toReturn;
     }
+
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
-        
+
     }
 }
-
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,4 +1,11 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
@@ -6,4 +13,28 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    public void testNewFile() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals("Test the links from test-file.md", List.of("https://something.com", "some-page.html"), links);
+    }
+
+    @Test
+    public void testImg() throws IOException {
+        Path fileName = Path.of("img.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals("Test the links from img.md", List.of(), links);
+    }
+
+    // @Test
+    // public void testfirstLine() throws IOException {
+    // Path fileName = Path.of("firstline.md");
+    // String contents = Files.readString(fileName);
+    // ArrayList<String> links = MarkdownParse.getLinks(contents);
+    // assertEquals("Test the links from firstline.md", List.of(""), links);
+    // }
 }
